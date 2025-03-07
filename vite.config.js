2026f9e3e57f31685f94aa67fd2135a016329ed3
Yi-Ping Jiang <apple80177@gmail.com>
2025-03-07 17:03:54 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/react-week5/'
+  base: '/react-week5/',
+  build: {
+    outDir: 'dist', // 確保這是正確的輸出目錄
+  }
 })
