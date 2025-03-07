b217a39bcfdf1d030572fe2fa13b5bb1089f6278
Yi-Ping Jiang <apple80177@gmail.com>
2025-03-07 17:46:16 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/react-week5/'
+  base: '/react-week5/',
+  build: {
+    outDir: 'dist',
+  },
 })
